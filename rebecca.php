d7e013bf5bbbeded898a589d6e40eacc7cc89dd7
rebecca-pesquet <rebecca.pesquet@gmail.com>
2020-09-03 16:21:34 +0200

    Commit

--- rebecca.php
+++ rebecca.php
@@ -1,6 +1,2 @@
 <?php
-echo '<h1> Cesi est le fichier de David & Rebecca</h1>';
-echo '<h1> Cesi est le fichier de David & Rebecca </h1>';
-echo '<h1>Function ajout article</h1>';
-echo '<h2>coucou O secours</h2>';
-echo '<h2>coucou O secours</h2>';
+echo '<h1> Coucou je suis mon code à moi</h1>';
