97854c7d8e435ca663a1665df1270a7b3658b368
Leo-Leclerc <leo.leclerc03@gmail.com>
2020-09-03 15:37:08 +0200

    g2 js

--- G2/g2.js
+++ G2/g2.js
@@ -1,12 +1,12 @@
 function changer_couleur_1() {
-    document.getElementById("leo").style.backgroundColor = "rgb(255,240,255)";
+    document.getElementById("leo").style.backgroundColor = "rgb(255,255,255)";
 }
 function changer_couleur_2() {
-    document.getElementById("julie").style.backgroundColor = "rgb(255, 240, 255)";
+    document.getElementById("julie").style.backgroundColor = "rgb(255, 255, 255)";
 }
 function changer_couleur_3() {
-    document.getElementById("julia").style.backgroundColor = "rgb(255, 240, 255)";
+    document.getElementById("julia").style.backgroundColor = "rgb(255, 255, 255)";
 }
 function changer_couleur_4() {
-    document.getElementById("capu").style.backgroundColor = "rgb(255, 240, 255)";
+    document.getElementById("capu").style.backgroundColor = "rgb(255, 255, 255)";
 }
