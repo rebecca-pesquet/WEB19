69786e0decd4a1ae8d3b1781fa6dcf62f907d452
alexandresolymos <51198792+alexandre427@users.noreply.github.com>
2020-09-03 14:56:08 +0200

    Modification html

--- G3/G3.php
+++ G3/G3.php
@@ -1,6 +1,16 @@
-<html>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Mathis, Alexandre & Rebecca !</title>
+    <link href="g3-style.css" rel="stylesheet" type="text/css">
+
+</head>
 <body>
-<title> Mathis, Alexandre & Rebecca !</title>
+
 <div class=dev>Point your mouse <A onmouseover="alert('Hello1')"
                                  href="">here</A> or <A onmouseover="alert('G3')" href="">here</A> or <A
             onmouseover="alert('...')" href="">here</A>.
