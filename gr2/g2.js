a16564004255754c4c138c3717414e5ecc2324f1
Leo-Leclerc <leo.leclerc03@gmail.com>
2020-09-03 14:54:08 +0200

    js g2

---
+++ gr2/g2.js
@@ -0,0 +1,12 @@
+function changer_couleur_1() {
+    document.getElementById("id_de_la_div").style.backgroundColor = rgba(0, 0, 0, 0.6);
+}
+function changer_couleur_2() {
+    document.getElementById("id_de_la_div").style.backgroundColor = rgba(0, 0, 0, 0.6);
+}
+function changer_couleur_3() {
+    document.getElementById("id_de_la_div").style.backgroundColor = rgba(0, 0, 0, 0.6);
+}
+function changer_couleur_4() {
+    document.getElementById("id_de_la_div").style.backgroundColor = rgba(0, 0, 0, 0.6);
+}
