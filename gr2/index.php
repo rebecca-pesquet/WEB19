87efd4406623dcf6ce1bb49d73db461c48e57db0
julia-leclerc <jleclerc.pro@gmail.com>
2020-09-03 15:06:34 +0200

    un test

--- gr2/index.php
+++ gr2/index.php
@@ -7,9 +7,9 @@
   <script src="g2.js"></script>
 </head>
 <body>
-    <div id="leo" onmouseover="changer_couleur_1()"></div>
-    <div id="julie" onmouseover="changer_couleur_2()"></div>
-    <div id="julia" onmouseover="changer_couleur_3()"></div>
-    <div id="capu" onmouseover="changer_couleur_4()"></div>
+    <div id="leo" onmouseover="changer_couleur_1()">Leo</div>
+    <div id="julie" onmouseover="changer_couleur_2()">Capu</div>
+    <div id="julia" onmouseover="changer_couleur_3()">Julia</div>
+    <div id="capu" onmouseover="changer_couleur_4()">Julie</div>
 </body>
 </html>
