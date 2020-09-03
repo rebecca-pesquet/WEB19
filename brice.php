ac2179b9fc82423a29be5a67a57eb2cdb772372f
breedan76 <brice@savbox.fr>
2020-09-03 14:08:39 +0200

    mon message

--- brice.php
+++ brice.php
@@ -1,6 +1,5 @@
 <?php
 
-echo '<h1>ceci est le fichier de  brice la vieille bique</h1>
-<h3>yeah man</h3>';
+echo '<h1>ceci est le fichier de  brice la vieille bique</h1>';
 echo '<h1>ceci est le fichier de  brice la vieille bique </h1>';
 echo '<h1>grosse folle </h1>';
