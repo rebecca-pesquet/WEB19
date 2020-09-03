3b3a88c2dcec1c273423fe715a1ae5f04db20ac1
alexandresolymos <51198792+alexandre427@users.noreply.github.com>
2020-09-03 15:19:28 +0200

    Modification link

--- G3/G3.php
+++ G3/G3.php
@@ -5,12 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <title> Mathis, Alexandre & Rebecca !</title>
     <link href="g3-style.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+
 
 </head>
 <body>
-<title> Mathis, Alexandre & Rebecca !</title>
 <div id="Rebecca">Rebecca</div>
 <div id="Mathis">Mathis</div>
 <div id="Alexandre">Alexandre</div>
