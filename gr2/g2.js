00e5e2120bfe70ed65abd1fb252a28ee5e94f9e8
Leo-Leclerc <leo.leclerc03@gmail.com>
2020-09-03 15:31:09 +0200

    sauce

--- gr2/g2.js
+++ gr2/g2.js
@@ -1,12 +1,12 @@
 function changer_couleur_1() {
-    document.getElementById("leo").style.backgroundColor = rgba(0, 0, 0);
+    document.getElementById("leo").style.backgroundColor = "rgb(255,255,255)";
 }
 function changer_couleur_2() {
-    document.getElementById("julie").style.backgroundColor = rgba(0, 0, 0);
+    document.getElementById("julie").style.backgroundColor = "rgb(255, 255, 255)";
 }
 function changer_couleur_3() {
-    document.getElementById("julia").style.backgroundColor = rgba(0, 0, 0);
+    document.getElementById("julia").style.backgroundColor = "rgb(255, 255, 255)";
 }
 function changer_couleur_4() {
-    document.getElementById("capu").style.backgroundColor = rgba(0, 0, 0);
+    document.getElementById("capu").style.backgroundColor = "rgb(255, 255, 255)";
 }
--- gr2/index.php
+++ gr2/index.php
@@ -4,12 +4,13 @@
   <meta charset="utf-8">
   <title>Groupe 2</title>
   <link rel="stylesheet" href="style-gr2.css">
-  <script src="g2.js"></script>
+    <link rel="script" href="g2.js">
 </head>
 <body>
     <div id="leo" onmouseover="changer_couleur_1()">Leo</div>
     <div id="julie" onmouseover="changer_couleur_2()">Capu</div>
     <div id="julia" onmouseover="changer_couleur_3()">Julia</div>
     <div id="capu" onmouseover="changer_couleur_4()">Julie</div>
+    <script type="text/javascript" src="/gr2/g2.js"></script>
 </body>
 </html>
