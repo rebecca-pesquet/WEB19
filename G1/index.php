6b715db5af21742f742dde8ad1e18bf825978e22
breedan76 <brice@savbox.fr>
2020-09-03 15:12:55 +0200

    mon message

--- G1/index.php
+++ G1/index.php
@@ -1,11 +1,28 @@
 <?php
 
-include 'index.css';
+
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="description" content="Des voyages pas cher mis en place par airbnb pour voyager à londres,amsterdam,paris,barcelone et plein d\'autres destinations à prix réduit">
+ <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+ 
+ <title>G1</title>
+ <link rel="stylesheet" href="index.css">
+    </head>
+
+<body>
+<h1> G1 </h1>
+<div id="aegir"> AEGIR</div>
+<div id="emma">EMMA </div>
+<div id="jeremy"> JEREMY</div>
+<div id="brice"> BRICE</div>
+</body>
+<script src="index.js" ></script>
+</html>';
 
 
-echo '<h1> G1 </h1>';
-
-echo '<div id="emma">EMMA </div>';
-echo '<div id="aegir"> AEGIR</div>';
-echo '<div id="jeremy"> JEREMY</div>';
-echo '<div id="brice"> BRICE</div>';
