e3ca0fa997c07e1facf82264c3d9009777b37da9
Leo-Leclerc <leo.leclerc03@gmail.com>
2020-09-03 15:03:42 +0200

    js g2

--- gr2/g2.js
+++ gr2/g2.js
@@ -1,12 +1,12 @@
 function changer_couleur_1() {
-    document.getElementById("id_de_la_div").style.backgroundColor = rgba(0, 0, 0, 0.6);
+    document.getElementById("leo").style.backgroundColor = rgba(0, 0, 0, 0.6);
 }
 function changer_couleur_2() {
-    document.getElementById("id_de_la_div").style.backgroundColor = rgba(0, 0, 0, 0.6);
+    document.getElementById("julie").style.backgroundColor = rgba(0, 0, 0, 0.6);
 }
 function changer_couleur_3() {
-    document.getElementById("id_de_la_div").style.backgroundColor = rgba(0, 0, 0, 0.6);
+    document.getElementById("julia").style.backgroundColor = rgba(0, 0, 0, 0.6);
 }
 function changer_couleur_4() {
-    document.getElementById("id_de_la_div").style.backgroundColor = rgba(0, 0, 0, 0.6);
+    document.getElementById("capu").style.backgroundColor = rgba(0, 0, 0, 0.6);
 }
--- gr2/index.php
+++ gr2/index.php
@@ -4,12 +4,13 @@
   <meta charset="utf-8">
   <title>Groupe 2</title>
   <link rel="stylesheet" href="style.css">
-  <script src="g2.js"></script>
+  <script src="script.js"></script>
 </head>
 <body>
-    <div id="leo">Leo</div>
-    <div id="julie">Julia</div>
-    <div id="julia">Julia</div>
-    <div id="capu">Capu</div>
+    <div id="leo" onmouseover="changer_couleur_1()"></div>
+    <div id="julie" onmouseover="changer_couleur_2()"></div>
+    <div id="julia" onmouseover="changer_couleur_3()"></div>
+    <div id="capu" onmouseover="changer_couleur_4()"></div>
 </body>
 </html>
+<script type="application/javascript" src="g2.js"></script>
