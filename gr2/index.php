213789bbbb44f5e3aea0f8b15e41d1d89a9266ec
julia-leclerc <jleclerc.pro@gmail.com>
2020-09-03 14:53:23 +0200

    un test

--- gr2/index.php
+++ gr2/index.php
@@ -1,1 +1,12 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Groupe 2</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+<div id="work"></div>
+</body>
+</html>
