3396faf30e275cc36bb4aa01570370e8b4ab8792
rebecca-pesquet <rebecca.pesquet@gmail.com>
2020-09-03 14:22:16 +0200

    conflit

--- david.php
+++ david.php
@@ -2,3 +2,4 @@
 echo '<h1>Ceci est le fichier de david</h1>';
 echo '<h1>Ceci est le super, méga, beau fichier de david</h1>';
 echo '<h2>Rebecca tu as déconné grave avec ton one-drive</h2>';
+echo '<h3>Coucou on est en conflit :</h3>';
